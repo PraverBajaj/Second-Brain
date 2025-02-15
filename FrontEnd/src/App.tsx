0b8bde4c73ec34327eb83f52291255f832a43106
Praver Bajaj <praverbajaj1609@gmail.com>
2025-02-15 23:14:16 +0530

    Added hash routing

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,18 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./pages/Signin";
 import SignUp from "./pages/signup";
 import LandingPage from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
+
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 

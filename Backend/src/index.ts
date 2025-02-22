d9b173834c9c244e459c40053bbe374d3dbaee24
Praver Bajaj <praverbajaj1609@gmail.com>
2025-02-23 03:45:14 +0530

    Changes for Readme

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -13,7 +13,7 @@ import DeleteUser from "./routes/delete";
 dotenv.config();
 const app = express();
 app.use(cors({
-    origin: "https://second-brain-rust.vercel.app",
+    origin: "https://second-brain-rust.vercel.app", // add your frontend url here 
     credentials: true,
 }));
 
